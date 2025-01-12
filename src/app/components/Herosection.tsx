74752e37671f81382556cb899e44854b896ae89a
Nickolas Dos Santos Cremasco <nickolascremasco@gmail.com>
2025-01-12 20:01:30 -0300

    making the slider

--- src/app/components/Herosection.tsx
+++ src/app/components/Herosection.tsx
@@ -1,18 +1,43 @@
 'use client'
-import { useState } from "react"
 
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/css';
+
+import { Navigation, Pagination } from "swiper/modules";
 export default function HeroSection() {
-    const [CurrentSlide, setCurrentSlide] = useState(0)
+    
+    const slides = [
+        'https://img.freepik.com/fotos-gratis/mulheres-felizes-de-encomendar-produtos-de-clientes-empresarios-que-trabalham-em-casa-em-um-backg-branco_1150-8107.jpg?ga=GA1.1.23163951.1734569073&semt=ais_hybrid',
+        'https://img.freepik.com/fotos-gratis/boneca-com-dinheiro_1160-17.jpg?ga=GA1.1.23163951.1734569073&semt=ais_hybrid',
+        'https://img.freepik.com/fotos-gratis/empresarios-em-branco_1385-2446.jpg?ga=GA1.1.23163951.1734569073&semt=ais_hybrid'
+    ];
 
+   
 
     return (
        <section className=" max-w-screen-xl mx-auto bg-sky-950">
-        {/* Slider */}
-            <div className="relative overflow-hidden max-w-screen-xl mx-auto">
-                {/* Slides */}
-                <div className="flex transition-transform duration-500 ease-in-out" style={{transform: `translateX(-${CurrentSlide * 100}%)`}}></div>
-                {slides}
-            </div>
+        {/* Sliders */}
+         <Swiper 
+            modules={[Navigation, Pagination]}
+            navigation
+            pagination={{ clickable:true}}
+            spaceBetween={50} 
+            slidesPerView={1}
+            loop={true}
+            className="w-1/2 float-right"
+        >
+            {/* Iterar sobre o array dos slides */}
+            {slides.map((slide, index) => (
+                <SwiperSlide key={index}>
+                    <img 
+                        src={slide} 
+                        alt={`Slide ${index + 1}`} 
+                        className="w-1/2 h-80 object-cover" 
+                    />
+                </SwiperSlide>
+            ))}
+         </Swiper>
        </section>
     )
 }
