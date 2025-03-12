223477a8ea9a821bc9ad2092d04f2cd6043e7774
Nickolas Dos Santos Cremasco <nickolascremasco@gmail.com>
2025-03-12 15:30:39 -0300

    testing

--- src/app/pages/article/page.tsx
+++ src/app/pages/article/page.tsx
@@ -4,6 +4,7 @@ import { Suspense } from "react"
 import Article from "@/app/components/Article"
 
 export default function Page() {
+    
     return (
         <Suspense fallback={<div>Carregando...</div>}>
             <Article />
