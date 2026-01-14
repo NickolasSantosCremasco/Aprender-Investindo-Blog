90681ddc03ce965cac7917a0d5d5c33e87766cf9
Nickolas Dos Santos Cremasco <nickolascremasco@gmail.com>
2026-01-14 08:08:49 -0300

    Setting Analytics and Font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body>
+      <body className={inter.className}>
         {children}
         <GoogleAnalytics gaId="G-ZH68Y25RVY" />
       </body>
